f57ea0b5aa07172b6d76c9a6c8ebf0d04d9d43d1
aliclarke206 <alison.clarke29@mail.dcu.ie>
2020-12-21 23:09:04 +0000

    added jasmine spies

--- specs/calcSpec.js
+++ specs/calcSpec.js
@@ -3,5 +3,13 @@ describe ("calculator", function(){
         it ("should return 42", function(){
             expect(addition(20, 22)).toBe(42);
         });
+        it("should return 26", function(){
+            expect(addition(19, 7)).toBe(26);
+        });
+        it("should return error if we dont supply two numbers", function(){
+            spyOn(window, "alert");
+            addition("Hitchhikers", "Guide");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
+        });
     });
 });
